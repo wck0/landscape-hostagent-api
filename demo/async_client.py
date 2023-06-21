df1918c947fc51e70cd670631a147ac69b5a40f1
Bill Kronholm <bill.kronholm@canonical.com>
2023-06-21 12:07:52 -0700

    Demo Windows hostagent client

--- demo/async_client.py
+++ demo/async_client.py
@@ -6,6 +6,109 @@ import hostagent_pb2
 import hostagent_pb2_grpc
 
 
+class FakeHostAgentClient:
+    token = "testprotoken"
+    uid = "testmachine-testuserid"
+    hostname = "testhostname"
+    instances = [
+        hostagent_pb2.HostAgentInfo.InstanceInfo(
+            id="testinstanceid",
+            name="testinstancename",
+            version_id="jammy",
+            instance_state=hostagent_pb2.InstanceState.Running,
+        ),
+        hostagent_pb2.HostAgentInfo.InstanceInfo(
+            id="testinstanceid2",
+            name="testinstancename2",
+            version_id="focal",
+            instance_state=hostagent_pb2.InstanceState.Stopped,
+        ),
+    ]
+
+    # see https://grpc.github.io/grpc/core/group__grpc__arg__keys.html
+    channel_options = {
+        # After a duration of this time the client/server pings its peer
+        # to see if the transport is still alive.
+        "grpc.keepalive_time_ms": 8_000,  # 8s
+        # After waiting for a duration of this time,
+        # if the keepalive ping sender does not receive the ping ack,
+        # it will close the transport.
+        "grpc.keepalive_timeout_ms": 5_000,  # 5s
+        # How many pings can the client send before needing to send a data/header frame?
+        "grpc.http2.max_pings_without_data": 5,
+        # Is it permissible to send keepalive pings from the client without any outstanding streams.
+        "grpc.keepalive_permit_without_calls": 1,
+    }
+
+    def __init__(self, token, uid, hostname, instances=None, channel_options=None):
+        self.token = token
+        self.uid = uid
+        self.hostname = hostname
+        if instances:
+            self.instances = instances
+        if channel_options:
+            self.channel_options.update(channel_options)
+
+    async def run(self):
+        print(f"Starting client {self.uid}")
+        async with grpc.aio.insecure_channel(
+            "localhost:50051", options=self.prepare_channel_options()
+        ) as channel:
+            await channel.channel_ready()
+            self.stub = hostagent_pb2_grpc.LandscapeHostAgentStub(channel)
+            await self.connect()
+
+    async def connect(self):
+        call = self.stub.Connect(self.generate_hostagent_info())
+        while True:
+            async for response in call:
+                await self.handle_response(response)
+
+            await asyncio.sleep(1)
+
+    def generate_hostagent_info(self):
+        yield hostagent_pb2.HostAgentInfo(
+            token=self.token,
+            uid=self.uid,
+            hostname=self.hostname,
+            instances=self.instances,
+        )
+
+    def prepare_channel_options(self):
+        return [(key, value) for key, value in self.channel_options.items()]
+
+    async def handle_response(self, response):
+        cmd = response.WhichOneof("cmd")
+        handler = None
+        if cmd:
+            handler = getattr(self, "handle_" + cmd, None)
+        if handler:
+            await handler(response)
+
+    async def handle_assign_host(self, response):
+        print("assign_host", response.assign_host.uid)
+        self.uid = response.assign_host.uid
+        await self.connect()
+
+    async def handle_install(self, response):
+        print("install", response.install.id)
+
+    async def handle_set_default(self, response):
+        print("set_default", response.set_default.id)
+
+    async def handle_shutdown_host(self, response):
+        print("shutdown_host", response.shutdown_host)
+
+    async def handle_start(self, response):
+        print("start", response.start.id)
+
+    async def handle_stop(self, response):
+        print("stop", response.stop.id)
+
+    async def handle_uninstall(self, response):
+        print("uninstall", response.uninstall.id)
+
+
 def generate_hostagentinfo():
     yield hostagent_pb2.HostAgentInfo(
         token="testprotoken",
@@ -28,18 +131,27 @@ def generate_hostagentinfo():
     )
 
 
-async def connect(stub):
-    call = stub.Connect(generate_hostagentinfo())
-    async for response in call:
-        print("client received", response)
+def get_uid():
+    # import uuid
+    # uid = uuid.uuid4().hex
+    # uid = "test-uid"
+    import string
+    import random
 
-
-async def run():
-    async with grpc.aio.insecure_channel("localhost:50051") as channel:
-        stub = hostagent_pb2_grpc.LandscapeHostAgentStub(channel)
-        await connect(stub)
+    uid = "".join(random.choices(string.ascii_lowercase, k=8))
+    return uid
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(run())
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-u", "--uid", type=str, default="", help="uid for the host agent"
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    uid = args.uid
+    client = FakeHostAgentClient("testprotoken", uid, "testhostname")
+    asyncio.run(client.run())
--- demo/async_server.py
+++ demo/async_server.py
@@ -1,46 +1,193 @@
 import asyncio
-import logging
+from configparser import ConfigParser
+import datetime
 import grpc
+import json
+import logging
+import urllib.parse
 
+import aio_pika
 import hostagent_pb2
 import hostagent_pb2_grpc
 
 
-class LandscapeHostAgentServicer(hostagent_pb2_grpc.LandscapeHostAgentServicer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+CONFIG_FILE = "demo/landscape_hostagent.conf"
 
-        self.commands = [
-            hostagent_pb2.Command(
-                install=hostagent_pb2.Command.Install(
-                    id="testid",
-                ),
-            )
-        ]
+
+def get_ampq_config():
+    config_parser = ConfigParser()
+    config_parser.read(CONFIG_FILE)
+    host = config_parser.get("rabbitmq", "host", fallback="localhost")
+    port = config_parser.get("rabbitmq", "port", fallback=5672)
+    user = config_parser.get("rabbitmq", "user", fallback="guest")
+    password = config_parser.get("rabbitmq", "password", fallback="guest")
+    vhost = config_parser.get("rabbitmq", "virtual_host", fallback="/")
+    # / is the default vhost for RabbitMQ and needs to be escaped as %2F
+    # so we override the safe parameter's default of "/"
+    quoted_vhost = urllib.parse.quote(vhost, safe="")
+    ampq_url = f"ampq://{user}:{password}@{host}:{port}/{quoted_vhost}"
+    task_queue = config_parser.get(
+        "rabbitmq",
+        "landscape_task_queue",
+        fallback="landscape-server-hostagent-task-queue",
+    )
+
+    return dict(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        vhost=vhost,
+        ampq_url=ampq_url,
+        task_queue=task_queue,
+    )
+
+
+def get_grpc_server_config():
+    # There are many grpc configuration parameters.
+    # See https://grpc.github.io/grpc/core/group__grpc__arg__keys.html
+    # They all start with "grpc." and the config file should match.
+    # Some values must be sent as integers and not as strings.
+    # We just convert all digit string values to integers,
+    # rather than use the `getint` method of `ConfigParser`
+    # so we don't have to write separate handling rules for every
+    # individual grpc configuration parameter.
+    config_parser = ConfigParser()
+    config_parser.read(CONFIG_FILE)
+    return [
+        (key, int(value)) if value.isdigit() else (key, value)
+        for key, value in config_parser.items("grpc")
+    ]
+
+
+class LandscapeHostAgentServicer(hostagent_pb2_grpc.LandscapeHostAgentServicer):
+    ampq_config = get_ampq_config()
+    server_options = get_grpc_server_config()
 
     async def Connect(self, request_iterator, context):
-        print("Connect request from client")
-        async for r in request_iterator:
-            print("Got request")
-            print(r)
+        print(type(context))
+        print(dir(context))
+        async for request in request_iterator:
+            if not request.uid:
+                command = self.assign_uid(request)
+                yield command
+            else:
+                await self.handle_request(request)
+                uid = request.uid
+                async for c in self.get_commands(uid):
+                    yield c
 
-        for c in self.commands:
-            print("Sending command")
-            print(c)
-            yield c
+    def assign_uid(self, request):
+        import uuid
+        uid = uuid.uuid4().hex
+        message = json.dumps({"assign_host": uid})
+        command = self.get_command(message)
+        return command
 
+    async def handle_request(self, request):
+        json_serialized_request = self.serialize_request(request)
+        print(json_serialized_request)
+        connection = await aio_pika.connect_robust(url=self.ampq_config["ampq_url"])
+        channel = await connection.channel()
+        await channel.declare_queue(self.ampq_config["task_queue"])
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body=json_serialized_request,
+            ),
+            routing_key=self.ampq_config["task_queue"],
+        )
+        await connection.close()
 
-async def serve():
-    server = grpc.aio.server()
-    hostagent_pb2_grpc.add_LandscapeHostAgentServicer_to_server(
-        LandscapeHostAgentServicer(),
-        server,
-    )
-    server.add_insecure_port("[::]:50051")
-    await server.start()
-    await server.wait_for_termination()
+    def serialize_request(self, request):
+        request_as_dict = dict(
+            hostname=request.hostname,
+            token=request.token,
+            uid=request.uid,
+            instances=self.serialize_instances(request.instances),
+        )
+        return json.dumps(request_as_dict).encode()
+
+    def serialize_instances(self, instances):
+        return [
+            dict(
+                id=instance.id,
+                instance_state=instance.instance_state,
+                name=instance.name,
+                version_id=instance.version_id,
+            )
+            for instance in instances
+        ]
+
+    async def get_commands(self, uid):
+        connection = await aio_pika.connect_robust(url=self.ampq_config["ampq_url"])
+
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(uid, auto_delete=False)
+
+            async with queue.iterator() as queue_iterator:
+                async for message in queue_iterator:
+                    async with message.process():
+                        yield self.get_command(message.body)
+
+    def get_command(self, message_body):
+        # TODO: handle fall through cases somehow
+        command_json = json.loads(message_body)
+        match command_json:
+            case {"assign_host": uid}:
+                command = hostagent_pb2.Command(
+                    assign_host=hostagent_pb2.Command.AssignHost(
+                        uid=uid,
+                    )
+                )
+            case {"install": id}:
+                command = hostagent_pb2.Command(
+                    install=hostagent_pb2.Command.Install(
+                        id=id,
+                    )
+                )
+            case {"set_default": id}:
+                command = hostagent_pb2.Command(
+                    set_default=hostagent_pb2.Command.SetDefault(
+                        id=id,
+                    )
+                )
+            case {"shutdown_host": None}:
+                command = hostagent_pb2.Command(
+                    shutdown_host=hostagent_pb2.Command.ShutdownHost()
+                )
+            case {"start": id}:
+                command = hostagent_pb2.Command(
+                    start=hostagent_pb2.Command.Start(
+                        id=id,
+                    )
+                )
+            case {"stop": id}:
+                command = hostagent_pb2.Command(
+                    stop=hostagent_pb2.Command.Stop(
+                        id=id,
+                    )
+                )
+            case {"uninstall": id}:
+                command = hostagent_pb2.Command(
+                    uninstall=hostagent_pb2.Command.Uninstall(
+                        id=id,
+                    )
+                )
+        return command
+
+    async def serve(self):
+        self._server = grpc.aio.server(options=self.server_options)
+        hostagent_pb2_grpc.add_LandscapeHostAgentServicer_to_server(
+            LandscapeHostAgentServicer(),
+            self._server,
+        )
+        self._server.add_insecure_port("[::]:50051")
+        await self._server.start()
+        await self._server.wait_for_termination()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(serve())
+    server = LandscapeHostAgentServicer()
+    asyncio.run(server.serve())
